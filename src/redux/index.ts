ff208b24cd0fa4a56fe75a4b43e524f7fa94abc6
quinv <vanquyjr11@gmail.com>
2024-04-17 22:21:10 +0700

    feat(costic): config persist redux

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,16 +1,26 @@
-import { Action, ThunkAction, configureStore } from '@reduxjs/toolkit';
-
+import { Action, ThunkAction, configureStore, combineReducers } from '@reduxjs/toolkit';
 import rootReducer from './rootSlice';
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const reducersCombine = combineReducers({
+  root: rootReducer,
+})
+const persistedReducer = persistReducer(persistConfig, reducersCombine)
 
 export const store = configureStore({
-  reducer: {
-    root: rootReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: false,
   }),
 });
 
+export const persistor = persistStore(store)
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export type AppThunk<ReturnType = void> = ThunkAction<
